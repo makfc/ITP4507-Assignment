f3d05ee507d4b559f3537ff49cb84c69f13817f4
makfc <makfc1234@gmail.com>
2017-11-13 20:11:03 +0800

    assignment change

--- src/Factory/CreateFoodCommandFactory.java
+++ src/Factory/CreateFoodCommandFactory.java
@@ -3,7 +3,6 @@ package Factory;
 import Command.*;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
 
@@ -28,6 +27,7 @@ public class CreateFoodCommandFactory extends CommandFactory {
                     (CreateFoodCommandFactory) Class.forName(
                     "Factory."+ ((String)foodFactoryMap.get(line))
             ).newInstance();
+            foodCommandFactory.setFoodItems(foodItems);
             command = foodCommandFactory.create();
         } catch (Exception e) {
             e.printStackTrace();
